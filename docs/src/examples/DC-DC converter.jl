7ba8b9c0b33972ce7c131bc961d89d2d503e28c4
SMattenet <sebastien.mattenet@uclouvain.be>
2022-09-13 13:05:21 +0200

    Add docstring for DCDC.System

--- docs/src/examples/DC-DC converter.jl
+++ docs/src/examples/DC-DC converter.jl
@@ -1,27 +1,7 @@
 using Test     #src
 # # Example: DC-DC converter
 #
-#md # [![Binder](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/generated/DC-DC converter.ipynb)
-#md # [![nbviewer](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/generated/DC-DC converter.ipynb)
-#
-# We consider a boost DC-DC converter which has been widely studied from the point of view of hybrid control, see for example in  [1, V.A],[2],[3].
-# This is a **safety problem** for a **switching system**.
-#
-# ![Boost DC-DC converter.](https://github.com/dionysos-dev/Dionysos.jl/blob/master/docs/assets/dcdcboost.jpg?raw=true)
-#
-# The state of the system is given by $x(t) = \begin{bmatrix} i_l(t) & v_c(t) \end{bmatrix}^\top$.
-# The switching system has two modes consisting in two-dimensional affine dynamics:
-# ```math
-# \dot{x} = f_p(x) = A_p x + b_p,\quad p=1,2
-# ```
-# with
-# ```math
-# A_1 = \begin{bmatrix} -\frac{r_l}{x_l} &0 \\ 0 & -\frac{1}{x_c}\frac{1}{r_0+r_c}  \end{bmatrix}, A_2= \begin{bmatrix} -\frac{1}{x_l}\left(r_l+\frac{r_0r_c}{r_0+r_c}\right) & -\frac{1}{x_l}\frac{r_0}{r_0+r_c}  \\ \frac{1}{x_c}\frac{r_0}{r_0+r_c}   & -\frac{1}{x_c}\frac{1}{r_0+r_c}  \end{bmatrix}, b = \begin{bmatrix} \frac{v_s}{x_l}\\0\end{bmatrix}.
-# ```
-# The goal is to design a controller to keep the state of the system in a safety region around the reference desired value, using as input only the switching
-# signal.
-#
-#
+# We consider the problem of a DCDC convertor.
 # In order to study the concrete system and its symbolic abstraction in a unified framework, we will solve the problem
 # for the sampled system with a sampling time $\tau$.
 #
@@ -52,6 +32,7 @@ const SY = DI.Symbolic
 # ### Definition of the system
 # we can import the module containing the DCDC problem like this 
 include(joinpath(dirname(dirname(pathof(Dionysos))), "problems", "DCDC.jl"))
+@doc DCDC.system
 
 # and we can instantiate the DC system with the provided system
 contsys=DCDC.system()
--- problems/DCDC.jl
+++ problems/DCDC.jl
@@ -14,6 +14,7 @@ const ST = DI.System
 const CO = DI.Control
 const SY = DI.Symbolic
 
+
 function dynamicofsystem(vs = 1.0, rL = 0.05, xL = 3.0, rC = 0.005, xC = 70.0, r0 = 1.0,ngrowthbound = 5)
     # Definition of the dynamics functions $f_p$ of the system:
     b = SVector(vs/xL, 0.0);
@@ -31,7 +32,28 @@ function dynamicofsystem(vs = 1.0, rL = 0.05, xL = 3.0, rC = 0.005, xC = 70.0, r
     return F_sys, L_growthbound,ngrowthbound
 end
 
+"""
+    system
 
+We consider a boost DC-DC converter which has been widely studied from the point of view of hybrid control, see for example in  [1, V.A],[2],[3].
+This is a **safety problem** for a **switching system**.
+
+![Boost DC-DC converter.](https://github.com/dionysos-dev/Dionysos.jl/blob/master/docs/assets/dcdcboost.jpg?raw=true)
+
+The state of the system is given by ``x(t) = \\begin{bmatrix} i_l(t) & v_c(t) \\end{bmatrix}^\\top``.
+The switching system has two modes consisting in two-dimensional affine dynamics:
+```math
+\\dot{x} = f_p(x) = A_p x + b_p,\\quad p=1,2
+```
+with
+```math
+A_1 = \\begin{bmatrix} - \\frac{r_l}{x_l} &0 \\\\ 0 & -\\frac{1}{x_c}\\frac{1}{r_0+r_c}  \\end{bmatrix},
+ A_2= \\begin{bmatrix} -\\frac{1}{x_l}\\left(r_l+\\frac{r_0r_c}{r_0+r_c}\\right) & -\\frac{1}{x_l}\\frac{r_0}{r_0+r_c} 
+ \\\\ \\frac{1}{x_c}\\frac{r_0}{r_0+r_c}   & -\\frac{1}{x_c}\\frac{1}{r_0+r_c}  \\end{bmatrix}, b = \\begin{bmatrix} \\frac{v_s}{x_l}\\\\0\\end{bmatrix}.
+```
+The goal is to design a controller to keep the state of the system in a safety region around the reference desired value, using as input only the switching
+signal.
+"""
 function system(
     sysnoise = SVector(0.0, 0.0),
     measnoise = SVector(0.0, 0.0),
@@ -83,6 +105,9 @@ function solveproblem(problem,x0 = SVector(0.0, 0.0), hx = SVector(2.0/4.0e3, 2.
     print("finished")
    # return contr
 end
-
-
+"""
+# ### References
+# 1. A. Girard, G. Pola and P. Tabuada, "Approximately Bisimilar Symbolic Models for Incrementally Stable Switched Systems," in IEEE Transactions on Automatic Control, vol. 55, no. 1, pp. 116-126, Jan. 2010.
+# 2. S. Mouelhi, A. Girard, and G. Gössler. “CoSyMA: a tool for controller synthesis using multi-scale abstractions”. In: HSCC. ACM. 2013, pp. 83–88.
+"""
 end
